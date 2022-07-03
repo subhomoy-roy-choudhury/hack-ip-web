eeed4c1649b25bdb37091c3400cf70233e8f8506
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-07-03 21:15:27 +0530

    added test page for testing gh-pages deployment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 import SystemDetails from './components/SystemDetails/SystemDetails.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+function Test() {
+  return (
+    <>
+    <h1>Test</h1>
+    </>
+  )
+}
+
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/hack-ip-web/" element={<Test />} />
         <Route path="/hack-ip-web/details/:encoded_string" element={<SystemDetails />} />
         {/* <Route path="/search" element={<SearchPage />} /> */}
         {/* <Route path="*" element={<Error />} /> */}
