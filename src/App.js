64777605b331ae97a0538591407352b082e77021
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-07-03 21:34:19 +0530

    removed testing components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import './App.css';
 import SystemDetails from './components/SystemDetails/SystemDetails.js';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function Test() {
-  return (
-    <>
-    <h1>Test</h1>
-    </>
-  )
-}
-
 function App() {
   return (
     <Router basename="/hack-ip-web/">
       <Routes>
-        <Route path="" element={<Test />} />
-        <Route path="details" element={<Test />} />
         <Route path="details/:encoded_string" element={<SystemDetails />} />
         {/* <Route path="/search" element={<SearchPage />} /> */}
         {/* <Route path="*" element={<Error />} /> */}
